f9b48faf34234013badcb2754b8a407b29428a7a
Neko_Kami <162889885+Neko0kami@users.noreply.github.com>
2024-08-21 15:08:27 +0000

    learnt variables

---
+++ 01_Basic/01_variables.js
@@ -0,0 +1,13 @@
+const account_id = 123
+let account_email = "123@gmail.com"
+var account_password = "1234"
+account_city = "Allahabad"
+
+//account_id = 321 // not allowed
+
+account_email = "321@gmail.com"
+account_password = "4321"
+account_city = "Prayagraj"
+
+console.log(account_id);
+console.table([account_email,account_password,account_city]);
