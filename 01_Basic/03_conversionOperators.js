e8bac67d6c399bd24cbab41b823e6d67b7fca468
Neko_Kami <162889885+Neko0kami@users.noreply.github.com>
2024-08-21 15:50:04 +0000

    conversion

---
+++ 01_Basic/03_conversionOperators.js
@@ -0,0 +1,31 @@
+let score = true
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+
+// "69" => 69
+// "69abc" => NaN
+// true => 1; false => 0
+// null => 0
+
+let isLoggedIn = 1
+
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(booleanisLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+
+let someNumber = 69
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
