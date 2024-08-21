97d1baeaf4f8b7b63463473fd84f43e8f67d024e
Neko_Kami <162889885+Neko0kami@users.noreply.github.com>
2024-08-21 15:12:37 +0000

    basic variables

--- 01_Basic/01_variables.js
+++ 01_Basic/01_variables.js
@@ -2,6 +2,7 @@ const account_id = 123
 let account_email = "123@gmail.com"
 var account_password = "1234"
 account_city = "Allahabad"
+let account_state;
 
 //account_id = 321 // not allowed
 
@@ -9,5 +10,10 @@ account_email = "321@gmail.com"
 account_password = "4321"
 account_city = "Prayagraj"
 
+/* 
+prefer not to use var
+because of issue in block scope and functional scope     
+*/
+
 console.log(account_id);
-console.table([account_email,account_password,account_city]);
+console.table([account_email, account_password, account_city, account_state]);
