c93c420a3bece8113f4e07cc4e32299362558a99
Neko_Kami <162889885+Neko0kami@users.noreply.github.com>
2024-08-21 15:34:44 +0000

    data types

---
+++ 01_Basic/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treats all js code as newer version
+
+// alert("hello") // using nodejs, not browser
+
+let name = "Aish"
+let age = 16
+let isLoggedin = true
+let state;
+
+// number (range) => 2 to the power 53
+// bigInt
+// string => ""
+// boolean => True/False
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object
+
+console.log(typeof null); // object
+
+console.log(typeof undefined); // undefined
