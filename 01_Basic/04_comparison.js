7a7e0e08926a4e31a4acf336b41837e6b4ccde4f
Neko_Kami <162889885+Neko0kami@users.noreply.github.com>
2024-08-22 14:19:47 +0000

    comparison

---
+++ 01_Basic/04_comparison.js
@@ -0,0 +1,15 @@
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);
+/* comparison(<,>,<=,>=) converts null to number */
+
+console.log(undefined>0);
+console.log(undefined<0);
+console.log(undefined==0);
+
+// === this checks strictly(also checks data type)
+console.log("2"===2);
+
