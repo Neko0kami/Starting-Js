959498f62ca5c29d469e462cb39ccdcb5752106c
Neko_Kami <162889885+Neko0kami@users.noreply.github.com>
2024-08-24 13:29:58 +0000

    maths and num

--- 01_Basic/06_nums_and_maths.js
+++ 01_Basic/06_nums_and_maths.js
@@ -1,6 +1,47 @@
 const score = 100
-console.log(score);
+// console.log(score);
 
 
 const balance = new Number(100)
-console.log(balance);
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(balance.toFixed(2));
+
+const num = 69.420
+// console.log(num.toPrecision(4));
+
+const hundred = 100000000
+// console.log(hundred.toLocaleString("en-Us"));
+
+// ######################  Maths ###################################
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+
+// console.log(Math.round(6.9));
+// console.log(Math.ceil(4.20));
+// console.log(Math.floor(6.9));
+// console.log(Math.min(6,9,4,2,0));
+// console.log(Math.max(6,9,4,2,0));
+
+console.log(Math.random());
+console.log((Math.random()*10));
+console.log(Math.floor(Math.random()*10)+1);
+
+const min  = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max - min +1))+min );
+
+
+
+
+
+
+
+
+
+
+
+
