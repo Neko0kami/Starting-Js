2a1ea796a50cc9c11ae012d34a0008c6c0d62864
Neko_Kami <162889885+Neko0kami@users.noreply.github.com>
2024-08-29 13:02:23 +0000

    scope

--- 03_Basic/02_scope.js
+++ 03_Basic/02_scope.js
@@ -5,11 +5,51 @@ if (true) {
     let a = 10;
     const b = 20;
     var c = 30;
-    console.log("Inner ",a);
+    // console.log("Inner ",a);
 }
 
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
 
+function one(){
+    const user = "abc";
+
+    function two(){
+        const site = "youtube";
+        // console.log(user);
+        
+    }
+    // console.log(website);
+
+    two()
+    
+}
+
+one()
+
+if(true){
+    const user = "abc";
+    if (user === "abc"){
+        const site = "youtube";
+        // console.log(user + " " + site);
+    }
+    // console.log(website);
+}
+
+// console.log(user);
+
+
+// ############################## interesting #############################
+
+function addone(num){
+    return num + 1;
+}
+
+console.log(addone(6));
+
+addtwo(5)
+const addtwo = function (num){
+    return num + 2;
+}
