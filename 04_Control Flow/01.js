0602010e0b560152262d8a5f9d5477d72879ed20
Neko_Kami <162889885+Neko0kami@users.noreply.github.com>
2024-08-31 15:57:11 +0000

    if statement

---
+++ 04_Control Flow/01.js
@@ -0,0 +1,50 @@
+// if
+
+let userLoggedIn = true;
+
+// if (userLoggedIn) {
+//     console.log("User is logged in");
+// }
+
+// >, <, ==, <=, >=, ===
+
+// let a = 10;
+// let b = 20;
+
+// if (a > b) {
+//     console.log("a is greater than b");
+// } else{
+//     console.log("b is greater than or equal to a");
+// }
+
+const balance = 1000;
+
+//if (balance>500) console.log(`balance is ${balance}`) <== (implicit scope)
+
+// if (balance < 500){
+//     console.log(`Insufficient balance. Please top up your account.`);
+// }else if (balance < 690){
+//     console.log(`Your account balance is below $690. Please consider adding funds.`);
+// }else{
+//     console.log(` Your account balance is ${balance}`);
+// }
+
+// $$ (and)
+
+let a = true;
+let b = false;
+let c = true;
+
+// $$ (and)
+
+if(a && c){
+    console.log("a and c ");
+}
+
+
+// || (or)
+
+if (a || b) {
+    console.log("a or b");
+}
+
