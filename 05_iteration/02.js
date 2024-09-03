eae9376c3ed7653959af4874227cc4c1ff9c20d8
Neko_Kami <162889885+Neko0kami@users.noreply.github.com>
2024-09-03 14:11:46 +0000

    while and do-while

---
+++ 05_iteration/02.js
@@ -0,0 +1,21 @@
+// while
+
+let i = 0;
+// while (i<10) {
+//     // console.log(i);   
+//     i++;
+// }
+
+// let arr = ["1", "2", "3", "4", "5", "6"];
+
+// while (i < arr.length) {
+//     console.log(`Value: ${arr[i]}`);
+//     i++;
+// }
+
+let score = 100;
+
+do {
+    console.log(`score ${score}`);
+    score += 100;
+} while (score < 1000);                   
