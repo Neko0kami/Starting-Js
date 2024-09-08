cb0c6ebbb0bce98fd653dbd0cecbf3d57201a6a0
Neko_Kami <162889885+Neko0kami@users.noreply.github.com>
2024-09-08 14:58:48 +0000

    Filter map

---
+++ 05_iteration/06.js
@@ -0,0 +1,23 @@
+// const arr = ["a", "b", "c", "d", "e"]
+
+// const value = arr.forEach(function (i){  // .forEach==> doesn't returns any value
+//     console.log(i);
+//     return i;
+// })
+
+// console.log(value);
+
+const nums = [1,2,3,4,5,6,7,8,9,10];
+// const values = nums.filter((num) => num > 4)
+
+// const nums2 =[]
+
+// nums.forEach((num) => {
+//     if(num > 4) {
+//         nums2.push(num);
+//     }
+// })
+
+
+// console.log(nums2);
+
